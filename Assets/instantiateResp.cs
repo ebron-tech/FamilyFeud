3ec7c5ce0c0e8318f851e543d9102c458a9deb70
Oscar Adrian <oadrian21@gmail.com>
2019-06-16 19:39:51 -0500

    First Commit

---
+++ Assets/instantiateResp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instantiateResp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		TMPro.TextMeshProUGUI t= gameObject.GetComponent<TMPro.TextMeshProUGUI>();
+		t.text= Application.dataPath ;
+		
+		Debug.Log("App dataPath Fixed: "+util.getDesktopDataPath("rounds"));
+		Debug.Log("Files:");
+		string[] filePaths = util.getDesktopDataFiles("rounds");
+		foreach(string file in filePaths){
+			Debug.Log("\t"+file);
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/util.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+public class util : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public static string getDesktopDataPath(string folderName){
+		string folderPath="";
+		#if UNITY_EDITOR
+		folderPath = Application.dataPath + "/StreamingAssets/"+folderName+"/";
+		#elif UNITY_WINRT
+		folderPath = Application.dataPath + "/"+folderName+"/";
+		#elif UNITY_STANDALONE_OSX
+		folderPath = new DirectoryInfo(Application.dataPath).Parent.Parent.ToString()+folderName+"/";
+		#endif
+		
+		return folderPath;
+	}
+	public static string[] getDesktopDataFiles(string folderName){
+		DirectoryInfo info = new DirectoryInfo(getDesktopDataPath(folderName));
+		FileInfo[] files= info.GetFiles();
+		//string[] filesStr= new string[files.Length];
+		List<string> filesStr = new List<string>();
+		for(int i =0;i<files.Length;i++){
+			string auxFile=files[i].ToString();
+			if(auxFile.EndsWith(".json")){
+				filesStr.Add(auxFile);
+			}else{
+				continue;
+			}
+		}
+		return filesStr.ToArray();
+	}
+
+	public static string getMobileDataPath (string DatabaseName){
+		#if UNITY_EDITOR
+		var dbPath = string.Format(@"Assets/StreamingAssets/{0}", DatabaseName);
+		#else
+		// check if file exists in Application.persistentDataPath
+		var filepath = string.Format("{0}/{1}", Application.persistentDataPath, DatabaseName);
+
+		if (!File.Exists(filepath))
+		{
+		Debug.Log("Database not in Persistent path");
+		// if it doesn't ->
+		// open StreamingAssets directory and load the db ->
+
+		#if UNITY_ANDROID 
+		var loadDb = new WWW("jar:file://" + Application.dataPath + "!/assets/" + DatabaseName);  // this is the path to your StreamingAssets in android
+		while (!loadDb.isDone) { }  // CAREFUL here, for safety reasons you shouldn't let this while loop unattended, place a timer and error check
+		// then save to Application.persistentDataPath
+		File.WriteAllBytes(filepath, loadDb.bytes);
+		#elif UNITY_IOS
+		var loadDb = Application.dataPath + "/Raw/" + DatabaseName;  // this is the path to your StreamingAssets in iOS
+		// then save to Application.persistentDataPath
+		File.Copy(loadDb, filepath);
+		#elif UNITY_WP8
+		var loadDb = Application.dataPath + "/StreamingAssets/" + DatabaseName;  // this is the path to your StreamingAssets in iOS
+		// then save to Application.persistentDataPath
+		File.Copy(loadDb, filepath);
+		#elif UNITY_WINRT
+		var loadDb = Application.dataPath + "/StreamingAssets/" + DatabaseName;  // this is the path to your StreamingAssets in iOS
+		// then save to Application.persistentDataPath
+		File.Copy(loadDb, filepath);
+		#else
+		var loadDb = Application.dataPath + "/StreamingAssets/" + DatabaseName;  // this is the path to your StreamingAssets in iOS
+		// then save to Application.persistentDataPath
+		File.Copy(loadDb, filepath);
+		#endif
+		}
+
+		var dbPath = filepath;
+		#endif
+		Debug.Log("Final PATH: " + dbPath);     
+		return dbPath;
+	}
+}
