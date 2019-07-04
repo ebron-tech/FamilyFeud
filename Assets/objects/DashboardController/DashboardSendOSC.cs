6ca2edd452953ecd89b288d08822f2fb0c4fccd7
Oscar Adrian <oadrian21@gmail.com>
2019-07-04 16:31:47 -0500

    removed first OSC package

--- Assets/objects/DashboardController/DashboardSendOSC.cs
+++ Assets/objects/DashboardController/DashboardSendOSC.cs
@@ -1,22 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using OscJack;
+
 public class DashboardSendOSC : MonoBehaviour
 {
-    OscClient client;
+//    OscClient client;
     public string Address="127.0.0.1";
     public int port =9001;
     void Start(){
-       client = new OscClient(Address, port);
+   //    client = new OscClient(Address, port);
     }
 
    public void selectAnswr(int id){
-        client.Send("/answr",id);
+    //    client.Send("/answr",id);
        // Debug.Log("SendOSC: answr "+id);
     }
 
     void OnApplicationQuit(){
-        client.Dispose();
+   //     client.Dispose();
     }
 }
--- Assets/scripts/BoardOSCinputs.cs
+++ Assets/scripts/BoardOSCinputs.cs
@@ -1,17 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using OscJack;
+//using OscJack;
 public class BoardOSCinputs : MonoBehaviour
 {
     // Start is called before the first frame update
     BoardManager boardManager;
-    OscServer server;
+    //OscServer server;
     void Awake(){
         boardManager = gameObject.GetComponent<BoardManager>();
-        server = new OscServer(9001);
+        //server = new OscServer(9001);
         
-        server.MessageDispatcher.AddCallback(
+        /*server.MessageDispatcher.AddCallback(
         "/answr", // OSC address
         (string address, OscDataHandle data) => {
             Debug.Log(string.Format("Recieved: answr ({0})",data.GetElementAsInt(0)));
@@ -19,11 +19,11 @@ public class BoardOSCinputs : MonoBehaviour
             
             }   
         );
-
+*/
 
     }
     void OnApplicationQuit(){
-        server.Dispose();
+     //   server.Dispose();
     }
 
     // Update is called once per frame
